76f9d1b6c8d9c5e1eac3a1125359b78bd7a4e6e8
Yo Yehudi <yochannah@gmail.com>
2016-04-22 12:24:10 +0100

    added icons and left nav bar. nonfunctional

--- src/cljs/gointermod/views.cljs
+++ src/cljs/gointermod/views.cljs
@@ -7,13 +7,40 @@
       [json-html.core :as json-html])
 (:use [json-html.core :only [edn->hiccup]]))
 
+(defn nav []
+  [:nav
+  [:h4 "Show results in:"]
+  [:ul
+    [:li
+     [:svg.icon [:use {:xlinkHref "#icon-summary"}]]
+     "Ortholog\u00A0Summary"]
+   [:li
+    [:svg.icon [:use {:xlinkHref "#icon-heatmap"}]]
+    "Interactive\u00A0Heatmap"]
+   [:li
+     [:svg.icon [:use {:xlinkHref "#icon-tree"}]]
+     "Ontology\u00A0Diagram"]]
+   [:h4 "Results filter:"]
+   [:ul
+    [:li
+     [:svg.icon [:use {:xlinkHref "#icon-biological-process"}]]
+     "Biological\u00A0Process"]
+   [:li
+     [:svg.icon [:use {:xlinkHref "#icon-molecular-function"}]]
+     "Molecular\u00A0Function"]
+   [:li
+     [:svg.icon [:use {:xlinkHref "#icon-cellular-component"}]]
+     "Cellular\u00A0Component"]]])
+
 
 (defn main-panel []
   (fn []
     [:div
       [icons/icons]
       [search/search]
-      [orthologs/orthologs]
+     [:main
+      [nav]
+      [orthologs/orthologs]]
       (when config/debug?
         [:div.db  (edn->hiccup @(re-frame/subscribe [:db]))])
     ]))
