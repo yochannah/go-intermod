5410e6331e2e158707776c4171e51b3553ada730
Yo Yehudi <yochannah@gmail.com>
2016-04-27 10:49:33 +0100

    tweaked query to accept multiple identifiers

--- src/cljs/gointermod/utils/comms.cljs
+++ src/cljs/gointermod/utils/comms.cljs
@@ -15,15 +15,20 @@
 (clj->js organism)))
 
 
-(defn make-base-query [identifier]
-  (str "<query model=\"genomic\" view=\"Gene.symbol Gene.secondaryIdentifier Gene.primaryIdentifier Gene.organism.name Gene.organism.taxonId Gene.homologues.homologue.primaryIdentifier Gene.homologues.homologue.secondaryIdentifier Gene.homologues.homologue.symbol Gene.homologues.homologue.organism.name Gene.homologues.homologue.organism.taxonId Gene.homologues.dataSets.name Gene.homologues.dataSets.url Gene.goAnnotation.evidence.code.code Gene.goAnnotation.evidence.publications.pubMedId Gene.goAnnotation.evidence.publications.title Gene.goAnnotation.ontologyTerm.identifier Gene.goAnnotation.ontologyTerm.name Gene.goAnnotation.ontologyTerm.namespace\" sortOrder=\"Gene.symbol ASC\" constraintLogic=\"B and C and A\" name=\"intermod_go\" > <constraint path=\"Gene.goAnnotation.qualifier\" op=\"IS NULL\" code=\"B\" />  <constraint path=\"Gene.goAnnotation.ontologyTerm.obsolete\" op=\"=\" value=\"false\" code=\"C\" />
-<constraint path=\"Gene.homologues.homologue.symbol\" op=\"=\" value=\"" identifier "\" code=\"A\" /></query>"))
+(defn make-base-query [identifier organism]
+  (str "<query model=\"genomic\" view=\"Gene.symbol Gene.secondaryIdentifier Gene.primaryIdentifier Gene.organism.name Gene.organism.taxonId Gene.homologues.homologue.primaryIdentifier Gene.homologues.homologue.secondaryIdentifier Gene.homologues.homologue.symbol Gene.homologues.homologue.organism.name Gene.homologues.homologue.organism.taxonId Gene.homologues.dataSets.name Gene.homologues.dataSets.url Gene.goAnnotation.evidence.code.code Gene.goAnnotation.evidence.publications.pubMedId Gene.goAnnotation.evidence.publications.title Gene.goAnnotation.ontologyTerm.identifier Gene.goAnnotation.ontologyTerm.name Gene.goAnnotation.ontologyTerm.namespace\" sortOrder=\"Gene.symbol ASC\" constraintLogic=\"B and C and D and  A\" name=\"intermod_go\" >
+    <constraint path=\"Gene.goAnnotation.qualifier\" op=\"IS NULL\" code=\"B\" />
+    <constraint path=\"Gene.goAnnotation.ontologyTerm.obsolete\" op=\"=\" value=\"false\" code=\"C\" />
+    <constraint path=\"Gene.homologues.homologue.organism.shortName\" code=\"D\" op=\"=\" value=\"" organism "\"/>
+    <constraint path=\"Gene\" code=\"A\" op=\"LOOKUP\" value=\"" identifier "\" extraValue=\"H. sapiens\"/>
+</query>"))
 
 (defn go-query
   "Get the results of GO term query for specified symbol/identifier"
   [organism identifier]
-  (let [service (get-service organism)
-        query (make-base-query identifier)]
+  (let [
+        service (get-service organism)
+        query (make-base-query identifier (get-abbrev :mouse))]
     (go (let [response (<! (http/post (str "http://" (.-root service) "/service/query/results")
        {:with-credentials? false
         :keywordize-keys? true
