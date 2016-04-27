2e9f51395e4b422bac2eba1205e14e6e44c33b22
Yo Yehudi <yochannah@gmail.com>
2016-04-27 13:44:59 +0100

    remove refactored out code

--- src/cljs/gointermod/search/handlers.cljs
+++ src/cljs/gointermod/search/handlers.cljs
@@ -42,20 +42,6 @@
     (assoc db :search-term term)
     ))
 
-(re-frame/register-handler
- ;
- :update-search-results
- (fn [db [_ search-results]]
-   (assoc db :search-results search-results)
-))
-
-(defn aggregate-by-species [search-results]
-  "helper to get the counts of aggregate go terms per organism / go term / ontology branch "
-  (reduce (fn [new-map [_ _ _ _ _ primary-id secondary-id symbol organism & args ]]
-    (update-in new-map [(keyword organism ) (keyword (utils/get-id primary-id secondary-id symbol)) (keyword (last args))] inc))
-      {} search-results)
-)
-
 (defn aggregate-by-orthologue [search-results]
   "helper to get the counts of aggregate go terms per organism / go term / ontology branch "
   (reduce (fn [new-map [_ _ _ _ _ primary-id secondary-id symbol organism & args ]]
@@ -64,17 +50,8 @@
 )
 
 (re-frame/register-handler
- :aggregate-search-results
- (fn [db [_ search-results]]
-   ;(.log js/console "Aggregate: (aggregate-by-species search-results)" (clj->js (aggregate-by-species search-results)))
-   (assoc db :aggregate-results (aggregate-by-species search-results)))
-)
-
-
-(re-frame/register-handler
  :concat-results
  (fn [db [_ search-results source]]
-;   (.log js/console (clj->js source) "Concat: " (clj->js (:results search-results)))
    (assoc-in db [:multi-mine-results source] search-results)
    (assoc-in db [:multi-mine-aggregate source] (aggregate-by-orthologue (:results search-results))))
 )
@@ -83,18 +60,7 @@
   ;;What do we do when a search button is pressed? This.
   :perform-search
   (fn [db [_ _]]
-    ;query humanmine
-    (go (let
-      [search-results (<! (comms/go-query (:selected-organism db) (:search-term db) :mouse))]
-        ;add results to the db
-      ;  (re-frame/dispatch [:update-search-results search-results])
-        (re-frame/dispatch [:aggregate-search-results (:results search-results)])
-
-          (comms/query-all-selected-organisms (:selected-organism db) (:search-term db))
-))
-    ;experimental multiples query
-;  (go (let [search-results (<! (comms/query-all-selected-organisms (:selected-organism db) (:search-term db)))]
-;        (.log js/console "Booya")
-;  ))
-
-    db))
+    ;asynchronously query all dem mines and add the results to the db
+    (go
+      (comms/query-all-selected-organisms (:selected-organism db) (:search-term db))
+) db))
