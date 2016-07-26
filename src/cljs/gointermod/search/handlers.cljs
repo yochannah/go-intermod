381ac2acc2d2ca8220f945171b82d0cb7ef2bcf6
Yo Yehudi <yochannah@gmail.com>
2016-07-26 14:17:32 +0100

    fix bug in which the heatmap was not loading properly due to not knowing which organism the original input gene query belonged to

--- src/cljs/gointermod/search/handlers.cljs
+++ src/cljs/gointermod/search/handlers.cljs
@@ -124,7 +124,7 @@
         :ortho-symbol na
         :ortho-secondary-id na
         :ortho-primary-id na
-        :ortho-organism na
+        :ortho-organism (get result 4)
         :original-db-id (get result 0)
         :original-symbol (get result 1)
         :original-secondary-id (get result 2)
